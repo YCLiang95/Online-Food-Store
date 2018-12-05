35082bc05d9847b7f2cd46b5a04d514a5b7e5849
ycliang <ycliang95@gmail.com>
2018-12-04 19:54:39 -0800

    back to ip

--- ui/script/request.js
+++ ui/script/request.js
@@ -1,5 +1,5 @@
-var serverURL = "http://localhost:8085/"
-//var serverURL = "http://47.254.94.78:8085/";
+//var serverURL = "http://localhost:8085/"
+var serverURL = "http://47.254.94.78:8085/";
 
 function postRequest(address, xhttp, data){
 	xhttp.open("POST", address + "cs160/mechandise/list", false);
